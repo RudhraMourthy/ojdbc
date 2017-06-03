c949c5862d850a1b093ffecd0a0361f6b364d756
RudhraMourthy <brudhramourthy@gmail.com>
2017-06-03 13:11:39 +0530

    Add files via upload

---
+++ Database.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.sql.*;
+public class Database {
+	
+	public static void main(String [] args) {
+		try
+		{
+			Connection con = null;
+	Class.forName("oracle.jdbc.driver.OracleDriver");
+	con = DriverManager.getConnection("Jdbc:Oracle:thin:@orcl.rmk.ac.in:1521:orcl","scott","tiger");
+	if(con != null)
+	System.out.println("Connection successfull");
+	else
+		System.out.println("Connection not successfull");	
+	con.close();
+	}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+	
+	
+
+}
+
